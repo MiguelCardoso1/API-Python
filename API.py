b0a64f281aed17971e44951845ec4221006ab3ed
Miguel Malton <miguel.malton@gmail.com>
2025-04-28 09:48:25 -0300

    Impórtação Flask/Lista de livros

--- API.py
+++ API.py
@@ -1,1 +1,21 @@
-print('Construção API')
+from flask import Flask, jsonify, request
+
+api=Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'Título':'Battlefield 3',
+        'Autor' : 'McNAB'
+    },
+    {
+        'id': 2,
+        'Título':'Harry Potter e a Pedra filosofal',
+        'Autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'Título' : 'Garota Exemplar',
+        'Autor' : 'Gillian Flynn'
+    },
+]
